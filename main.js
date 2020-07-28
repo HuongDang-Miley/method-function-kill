53bb5f7370aad15619ad52c84c62c4154422bd61
Huong Dang <miley.dawn@codeimmersives.com>
2020-07-28 15:42:58 -0400

    HW

--- main.js
+++ main.js
@@ -2,6 +2,35 @@
  * YOUR CODE HERE *
  ******************/
 
+const newPerson = function (firstName = 'Anonymous', lastName = 'Person', age, married = false) {
+  return {
+    firstName,
+    lastName,
+    age,
+    married,
+    goingOn: function() {
+      return this.age + 1
+    },
+    ageUp: function() {
+      this.age++
+    },
+    getFullName: function() {
+      return `${this.firstName} ${this.lastName}`
+    },
+    marry: function(newPerson) {
+      this.married = true
+      newPerson.married = true
+      this.spouseName = newPerson.getFullName()
+      newPerson.spouseName = this.getFullName()
+    },
+    divorce: function(newPerson) {
+      this.married = false
+      newPerson.married = false
+      delete this.spouseName
+      delete newPerson.spouseName
+    }
+   }
+}
 
 
 /*********************************
